9d8a22541176e86468efbca78fa5f7e8eb5c5db9
Yellapu MeghaShyam <22l61a6118@cec.ac.in>
2025-07-24 22:07:23 +0530

    Update socket.js

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -5,12 +5,17 @@ import express from "express";
 const app = express();
 const server = http.createServer(app);
 
+// const io = new Server(server, {
+//   cors: {
+//     origin: ["http://localhost:5173","https://chatty-frontend-xi.vercel.app"],
+//   },
+// });
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173","https://chatty-frontend-xi.vercel.app"],
+    origin: ["http://localhost:5173", "https://chatty-frontend-xi.vercel.app"],
+    credentials: true, // ✅ Allow credentials (very important!)
   },
 });
-
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
